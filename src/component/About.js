ce3b535d9c332eeb1a7aa08e676ab0a6706eea54
Kameshwar Das <46256967+kameshwar44@users.noreply.github.com>
2023-01-05 16:27:37 +0530

    Add files via upload

---
+++ src/component/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Home.css'
+const About = () => {
+  return (
+    <>
+    <div className='main'>
+      
+        <div className='center'>
+        <h1>Create Your Portfolio Now</h1>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKEncvG8tRTMruAGkh7Su3s6_9KVy1niQwNA&usqp=CAU" alt="random.img" />
+        <p>As a web designer, it is essential for you to have a website portfolio to display your best work and provide others with an outlet to reach you. An updated website portfolio can be used to help you gain employment, find new opportunities, and become a freelancer.</p>
+        </div>
+
+    </div>
+
+    </>
+    
+  )
+}
+
+export default About
---
+++ src/component/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+const Home = () => {
+  return (
+    <>
+    <div className='main'>
+      <p className='fon'>Welcome to Genisys</p>
+    </div>
+
+    </>
+
+  )
+}
+
+export default Home
